1c333f53a0a3e0db773fd26aa8c730125492f081
Lukasz Guminski <lguminski@gmail.com>
2020-03-19 23:52:16 +0100

    added releases file

---
+++ config/releases.exs
@@ -0,0 +1,13 @@
+import Config
+
+config :hello_world, HelloWorldWeb.Endpoint,
+  server: true,
+  url: [host: "localhost", port: 4001],
+  http: [port: 4000],
+  https: [
+    port: 4001,
+    cipher_suite: :strong,
+    certfile: "priv/cert/selfsigned.pem",
+    keyfile: "priv/cert/selfsigned_key.pem"
+  ],
+  force_ssl: [hsts: true, host: nil, log: :debug, exclude: []]
